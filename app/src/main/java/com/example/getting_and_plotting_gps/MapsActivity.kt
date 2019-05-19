15703fb00da60593209fe02d9477bd71a6c7be9d
Rafael Lindoso <rafael.lindoso@gmail.com>
2019-05-19 19:03:56 -0300

    Pedindo permissão para pegar localização Pegando a localização e exibindo marcação no mapa

--- app/src/main/java/com/example/getting_and_plotting_gps/MapsActivity.kt
+++ app/src/main/java/com/example/getting_and_plotting_gps/MapsActivity.kt
@@ -1,14 +1,26 @@
 package com.example.getting_and_plotting_gps
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
-
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import android.content.pm.PackageManager
+import android.widget.Toast
+
+
+
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -18,6 +30,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            1)
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -34,10 +49,51 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+    }
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            1 -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                    // Define a listener that responds to location updates
+                    val locationListener = object : LocationListener {
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+                        override fun onLocationChanged(location: Location) {
+                            // Called when a new location is found by the network location provider.
+                            makeUseOfNewLocation(location)
+                        }
+
+                        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+                        }
+
+                        override fun onProviderEnabled(provider: String) {
+                        }
+
+                        override fun onProviderDisabled(provider: String) {
+                        }
+                    }
+                    // Register the listener with the Location Manager to receive location updates
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0f,locationListener)
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    val toast = Toast.makeText(this, "O app não funciona sem permição de localização", Toast.LENGTH_LONG)
+                    toast.show()
+                }
+                return
+            }
+        }// other 'case' lines to check for other
+        // permissions this app might request
+    }
+
+    fun makeUseOfNewLocation(location: Location){
+        // Add a marker in local and move the camera
+        val local = LatLng(location.latitude, location.longitude)
+        mMap.addMarker(MarkerOptions().position(local).title(""))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(local))
+
     }
 }
